fb0dc43966898325b02f4fd99ad1858bdc6874bb
Shushlayka2 <inashur68@gmail.com>
2018-10-28 22:56:33 +0300

    models generated from db

---
+++ db.py
@@ -0,0 +1,73 @@
+# coding: utf-8
+from sqlalchemy import Boolean, Column, Date, ForeignKey, Integer, String, Text, text
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+metadata = Base.metadata
+
+
+class Institute(Base):
+    __tablename__ = 'institute'
+
+    id = Column(Integer, primary_key=True, unique=True, server_default=text("nextval('institute_id_seq'::regclass)"))
+    name = Column(String(40))
+
+
+class Scope(Base):
+    __tablename__ = 'scope'
+
+    id = Column(Integer, primary_key=True, unique=True, server_default=text("nextval('scope_id_seq'::regclass)"))
+    name = Column(String(30), nullable=False)
+
+
+class Department(Base):
+    __tablename__ = 'department'
+
+    id = Column(Integer, primary_key=True, unique=True, server_default=text("nextval('department_id_seq'::regclass)"))
+    name = Column(String(40))
+    institute_id = Column(ForeignKey('institute.id'), nullable=False)
+
+    institute = relationship('Institute')
+
+
+class Professor(Base):
+    __tablename__ = 'professor'
+
+    full_name = Column(String(60), nullable=False)
+    id = Column(Integer, primary_key=True, unique=True, server_default=text("nextval('professor_id_seq'::regclass)"))
+    is_expert = Column(Boolean, nullable=False, server_default=text("false"))
+    department_id = Column(ForeignKey('department.id'), nullable=False)
+
+    department = relationship('Department')
+
+
+class Diploma(Base):
+    __tablename__ = 'diploma'
+
+    id = Column(Integer, primary_key=True, unique=True, server_default=text("nextval('diploma_id_seq'::regclass)"))
+    thesis = Column(String(90), nullable=False)
+    description = Column(Text, nullable=False)
+    deadline = Column(Date, nullable=False)
+    team_work = Column(Boolean, nullable=False, server_default=text("false"))
+    evaluation = Column(Integer)
+    professor_id = Column(ForeignKey('professor.id'), nullable=False)
+    scope_id = Column(ForeignKey('scope.id'))
+
+    professor = relationship('Professor')
+    scope = relationship('Scope')
+
+
+class Criterion(Base):
+    __tablename__ = 'criteria'
+
+    id = Column(Integer, primary_key=True, unique=True, server_default=text("nextval('criteria_id_seq'::regclass)"))
+    thesis_ev = Column(Integer, nullable=False)
+    relevance_ev = Column(Integer, nullable=False)
+    interest_ev = Column(Integer, nullable=False)
+    feasibility_ev = Column(Integer, nullable=False)
+    expert_id = Column(ForeignKey('professor.id'), nullable=False)
+    diploma_id = Column(ForeignKey('diploma.id'), nullable=False)
+
+    diploma = relationship('Diploma')
+    expert = relationship('Professor')
